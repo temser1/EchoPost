06e5418da6021d79699cb86177c6c26861fb8138
Serovtsev Artem <1doncarleon@gmail.com>
2023-05-18 16:13:25 +0300

    add

---
+++ src/test/java/ru/netology/echo/PostmanTest.java
@@ -0,0 +1,25 @@
+package ru.neology.echo;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanTest {
+    @Test
+    void postmanTest1() {
+        given()
+                .baseUri("https://postman-echo.com")
+                //.contentType("text/plain; charset=UTF-8")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        //.body("some data")
+        ;
+    }
+}
